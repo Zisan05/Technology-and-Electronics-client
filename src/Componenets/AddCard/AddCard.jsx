0d3506a58cd8e25887d52c5fcf110118a747f379
Zisan05 <mdzisanislam69@gmail.com>
2023-10-19 07:54:07 -0700

    Delete card part done

--- src/Componenets/AddCard/AddCard.jsx
+++ src/Componenets/AddCard/AddCard.jsx
@@ -1,17 +1,19 @@
 import { useLoaderData } from "react-router-dom";
 import AddCards from "../AddCards/AddCards";
+import { useState } from "react";
 
 
 const AddCard = () => {
 
-    const product = useLoaderData();
+    const addcard = useLoaderData();
+    const [product,setProduct] =useState(addcard)
 
 
     return (
         <div className="grid  lg:grid-cols-2 gap-[30px] md:ml-[130px] lg:ml-[30px] mt-[30px]">
             {
                 product.map(data =><AddCards key ={data.id}
-                data = {data}></AddCards> )
+                data = {data} product ={product} setProduct ={setProduct}></AddCards> )
             }
         </div>
     );
--- src/Componenets/AddCards/AddCards.jsx
+++ src/Componenets/AddCards/AddCards.jsx
@@ -1,11 +1,32 @@
+import Swal from "sweetalert2";
 
-const AddCards = ({data}) => {
+const AddCards = ({data,setProduct,product}) => {
 
-    const {image,name,brandname,productType,price,rating,shortDescription} = data
+    const {_id,image,name,brandname,productType,price,rating,shortDescription} = data;
+
+    const handleDelete = (id) => {
+     fetch(`http://localhost:5000/card/${id}`,{
+      method: 'DELETE'
+     })
+     .then (res => res.json())
+     .then (data => {
+      if(data.deletedCount>0){
+
+        const remaining = product.filter(item => item._id !== id )
+        setProduct(remaining);
+        Swal.fire(
+          'success',
+          'Successfully added your Item',
+          'success'
+        )
+      }
+     })
+      
+    }
     return (
         <div>
             <div className="card card-side md:h-[350px] lg:h-[350px] lg:w-[500px] bg-blue-300 shadow-xl">
-  <figure><img className="h-[200px] ml-[30px]" src={image} alt="item"/></figure>
+  <figure><img className="h-[200px] w-[200px] ml-[30px]" src={image} alt="item"/></figure>
   <div className="card-body mt-[40px]">
     <h2 className="card-title">Name: {name}</h2>
     <h2 className="card-title">Brand Name: {brandname}</h2>
@@ -13,7 +34,7 @@ const AddCards = ({data}) => {
     <h2 className="card-title">Price: ${price}</h2>
     <h2 className="card-title">Rating:{rating}/10</h2>
        <div>
-        <button className="btn bg-purple-400">Delete</button>
+        <button onClick={() => handleDelete(_id)} className="btn bg-purple-400">Delete</button>
        </div>
   </div>
 </div>
